7fe5ccec4062200023adedaeaeb9a231faed83bd
alvaroOrrego <alvaro.orrego.m@gmail.com>
2023-06-06 17:12:20 -0400

    intento 1 de arreglo detalles cortes en diagonal + adicion de funcionalidad form contacto

---
+++ assets/php/contacto.php
@@ -0,0 +1,19 @@
+<?php
+    $destinatario = 'alvaro.orrego.m@gmail.com';
+    $nombre = $_POST['nombre'];
+/*     $apellido = $_POST['apellido'];
+    $asunto = $_POST['asunto']; */
+    $email = $_POST['email'];
+    $mensaje = $_POST['msj'];
+
+    $header = "Enviado desde el sitio web de NOOW\n" . "Nombre del emisor: " . $nombre . "\nEmail del emisor:\n" . $email;
+    $msjCompleto = "Mensaje:\n" . $mensaje . "\nSaluda atentamente: " . $nombre;
+
+    $sentMail = mail($destinatario, $msjCompleto, $header);
+    if($sentMail){       
+        echo"<script>alert('Mensaje enviado correctamente!')</script>";
+        echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+    }else{
+        echo "<script>alert('Error, mensaje fallido...')</script>";
+    }
+?>
