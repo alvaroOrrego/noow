a27b5d9ef954011fbb001a8b6ecc268acd374abf
catalinaCastilloB <ccastillobelmar@gmail.com>
2023-06-07 16:01:34 -0400

    seccion pilares

--- assets/js/scriptAnimation.js
+++ assets/js/scriptAnimation.js
@@ -100,3 +100,103 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
     } // else was horizontal scroll
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
  }, false);
+
+
+
+ /* animacion de los pilares */
+
+const progreso    = document.getElementById("progreso");
+const anterior    = document.getElementById("anterior");
+const siguiente   = document.getElementById("siguiente");
+const circles     = document.querySelectorAll(".circle");
+const pages       = document.querySelectorAll(".P");
+const parrafos    = document.querySelectorAll(".Pilar");
+
+let currentActive = 1;
+let lastActive = 1;
+
+/* Boton siguiente, despliega la ventana que se indica*/
+siguiente.addEventListener("click", () => {
+    lastActive = currentActive;
+    currentActive++;
+
+    if (currentActive > circles.length){
+        currentActive = circles.length;
+    };
+
+    /* boton activo o desactivo */
+    anterior.disabled=false;
+
+    if(currentActive != circles.length){
+       siguiente.disabled=false;
+    }else if(currentActive == circles.length){
+        siguiente.disabled=true;
+     }
+
+
+    pilarActivo();
+    displayVentana();
+})
+
+/* Boton anterior, despliega la ventana que se indica*/
+anterior.addEventListener("click", () => {
+    lastActive = currentActive;
+    currentActive--;
+
+    
+    if (currentActive < 1){
+        currentActive = 1;
+    };
+
+    /* boton activo o desactivo */
+    siguiente.disabled=false;
+
+    if(currentActive != 1){
+        anterior.disabled=false;
+        
+     }
+     else if(currentActive ==1){
+        anterior.disabled=true;
+     }
+
+
+
+    pilarActivo();
+    displayVentana();
+})
+
+/*quita ventana que se encuentra en display y coloca la nueva ventana */
+function displayVentana(){
+    let lastPage = pages[lastActive-1];
+    let page = pages[currentActive-1];
+    let parrafo = parrafos[currentActive-1];
+    let lastParrafo = parrafos[lastActive-1];
+    page.classList.add("show");
+    parrafo.classList.add("show");
+    
+    if(lastActive != currentActive){
+    lastPage.classList.remove("show");
+
+    lastParrafo.classList.remove("show");
+}
+}
+
+/* modifica la barra de avance, correspondiente a la ventana activa*/
+function pilarActivo(){
+    circles.forEach((circle,i)=>{
+            let page = pages[i];
+            if(i < currentActive){
+                circle.classList.add("active");
+            }
+            else{
+                circle.classList.remove("active");
+            }
+    })
+
+    
+
+    const activos = document.querySelectorAll(".circle.active");
+    console.log(activos);
+    progreso.style.width = (((activos.length-1)/(circles.length-1)) * 100 + '%');
+
+}
